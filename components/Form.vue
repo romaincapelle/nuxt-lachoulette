890c839e5dba15777c3495f70a605446d3fd27a8
romaincapelle <romain.capelle@gmail.com>
2020-06-01 17:39:36 -0400

    fixing contact form

--- components/Form.vue
+++ components/Form.vue
@@ -1,42 +1,49 @@
 <template>
-  <form
-    class="px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md"
-    name="ask-question"
-    action="/thanks"
-    method="post"
-    data-netlify="true"
-    data-netlify-honeypot="bot-field"
-  >
-    <input type="hidden" name="form-name" value="ask-question" />
-    <label
-      v-for="(panelist, index) in panelists"
-      :key="index"
-      class="block mb-2 text-sm font-bold text-gray-700"
-    >
+  <form @submit.prevent="handleSubmit">
+    <label v-for="(panelist, index) in panelists" :key="index">
       <input
         type="radio"
         name="panelist"
         :value="panelist"
-        :checked="panelist === currentPanelist"
-        @input="(ev) => updatePanelist"
+        :checked="form.askPerson === panelist"
+        @input="(ev) => (form.askPerson = ev.target.value)"
       />
       <span>{{ panelist }}</span>
     </label>
-    <button>Submit</button>
   </form>
 </template>
 <script>
+import axios from 'axios'
+
 export default {
   name: 'QAForm',
   data() {
     return {
-      panelists: ['Evan You', 'Chris Fritz'],
-      currentPanelist: 'Evan You'
+      form: {
+        askPerson: ''
+      }
     }
   },
   methods: {
-    updatePanelist(ev) {
-      this.currentPanelist = ev.target.value
+    encode(data) {
+      return Object.keys(data)
+        .map(
+          (key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+        )
+        .join('&')
+    },
+    handleSubmit() {
+      const axiosConfig = {
+        header: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      }
+      axios.post(
+        '/',
+        this.encode({
+          'form-name': 'ask-question',
+          ...this.form
+        }),
+        axiosConfig
+      )
     }
   }
 }
