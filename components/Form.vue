18f07700e679c7cf7b40d50e20c28a42b92286fb
romaincapelle <romain.capelle@gmail.com>
2020-06-01 14:46:07 -0400

    first commit

---
+++ components/Form.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <form
+      name="contactus"
+      action="/thanks"
+      method="post"
+      netlify
+      netlify-honeypot="bot-field"
+    >
+      <input type="hidden" name="form-name" value="contactus" />
+      <div>
+        <label for="name">Name:</label>
+        <input class="border" type="text" name="name" required />
+      </div>
+      <div>
+        <input class="border" type="email" name="email" required />
+        <label for="email">Email:</label>
+      </div>
+      <div>
+        <textarea class="border" name="message" required></textarea>
+        <label for="message">Message:</label>
+      </div>
+      <button class="border" type="submit" value="Send message">Send</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
