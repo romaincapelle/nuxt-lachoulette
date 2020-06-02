7f84d7b30d0a44a9fb6192635c9e46c5302eae6f
romaincapelle <romain.capelle@gmail.com>
2020-06-02 12:45:28 -0400

    fixing contact form

--- components/Form02.vue
+++ components/Form02.vue
@@ -52,7 +52,6 @@
           <option value="eur" name="eur">EUR</option>
         </select>
       </div>
-      <p 
       <button
         class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
         type="submit"
---
+++ pages/form02page.vue
@@ -0,0 +1,12 @@
+<template>
+  <form02 />
+</template>
+
+<script>
+import form02 from '../components/Form02'
+export default {
+  components: { form02 }
+}
+</script>
+
+<style></style>
